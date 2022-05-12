04e42e15cddcb5eb4d13e5b7c80f7f51fb9db198
Mykhailo Hrytskov <mykhailo.hrytskov@dish.com>
2022-05-12 13:43:20 +0300

    Added firebase

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, set, ref, onValue } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhQbnYErbsQe37RAkDY-1jXo1xNswakIw",
+  authDomain: "miksa-friends-chat.firebaseapp.com",
+  projectId: "miksa-friends-chat",
+  storageBucket: "miksa-friends-chat.appspot.com",
+  messagingSenderId: "649844966884",
+  appId: "1:649844966884:web:b8be7f42863209e1af73f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+function storeHighScore(userId, score) {
+  const db = getDatabase();
+  const reference = ref(db, 'users/' + userId);
+  set(reference, {
+    highscore: score,
+  });
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={() => storeHighScore(123123, 5414124)}
+      >
+        <Text>Set score</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
