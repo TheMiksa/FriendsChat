cbeae3bb31b02a8d6372a10ae29f7b5a4e989822
Mykhailo Hrytskov <mykhailo.hrytskov@dish.com>
2022-05-12 17:27:43 +0300

    Added ability to set name, send message, get list messages and rendering of message list

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, TextInput, FlatList } from 'react-native';
 import { initializeApp } from 'firebase/app';
-import { getDatabase, set, ref, onValue } from 'firebase/database';
+import { getDatabase, push, ref, onValue, orderByKey } from 'firebase/database';
+import { forIn } from 'lodash';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDhQbnYErbsQe37RAkDY-1jXo1xNswakIw",
@@ -14,23 +16,126 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-function storeHighScore(userId, score) {
-  const db = getDatabase();
-  const reference = ref(db, 'users/' + userId);
-  set(reference, {
-    highscore: score,
+const database = getDatabase();
+function sendMessage(userId: string, text: string, msTime: number) {
+  const reference = ref(database, 'friends-chat/messages');
+  push(reference, {
+    message: text,
+    userId,
+    msTime,
   });
 }
 
+const getMessages = () => {
+  const db = getDatabase();
+
+
+  console.log('----db: ', db);
+}
+
+type Message = {
+  userId: string,
+  id: string,
+  message: string,
+  msTime: number,
+}
+type Message2 = {
+  userId: string,
+  message: string,
+  msTime: number,
+}
 export default function App() {
+  const [messages, setMessages] = useState<Array<Message>>([]);
+  const [text, setText] = useState('34234sfsfsdfsfsfsdfdsf');
+  const [userName, setUserName] = useState('');
+  const [isNameEditing, setIsNameEditing] = useState(true);
+
+  const messagesRef = ref(database, 'friends-chat/messages');
+  
+  useEffect(() => {
+    onValue(messagesRef, (s) => {
+      const v = s.val();
+
+      const newMessages: Array<Message> = [];
+      forIn(v, (val: Message2, key: string) => {
+        newMessages.push({
+          userId: val.userId,
+          message: val.message,
+          msTime: val.msTime,
+          id: key,
+        });
+      })
+      setMessages(newMessages);
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        {userName.length < 1 || isNameEditing ? (
+          <View
+            style={{
+              backgroundColor: '#f0f0f0',
+            }}
+          >
+          <TextInput
+          onChangeText={(value: string) => setUserName(value.trim())}
+          value={userName}
+          style={{
+            backgroundColor: '#e0e0e0',
+          }}
+          />
+          <TouchableOpacity
+          style={{
+            backgroundColor: '#eeee0',
+          }}
+          onPress={() => setIsNameEditing(false)}
+        >
+          <Text>Set your Name</Text>
+        </TouchableOpacity>
+        </View>
+        ) : (
+          <View>
+          <Text>{userName}</Text>
+          <TouchableOpacity
+            onPress={() => setIsNameEditing(true)}
+          >
+            <Text>Edit Name</Text>
+          </TouchableOpacity>
+      <View style={{
+        height: 500,
+      }}>
+      <Text>Messages:</Text>
+        <FlatList
+          renderItem={(value) => {
+            const item: Message = value.item;
+            return (
+            <View
+              key={item.id}
+              style={{
+                backgroundColor: 'tomato',
+                margin: 4,
+              }}
+            >
+              <Text>{item.userId}:</Text>
+              <Text>{item.message}</Text>
+              <Text>{item.msTime}</Text>
+            </View>
+          );
+        }}
+          data={messages}
+          style={{
+            backgroundColor: 'olive',
+          }}
+        />
+      </View>
       <TouchableOpacity
-        onPress={() => storeHighScore(123123, 5414124)}
+        onPress={() => sendMessage(userName, text, Date.now())}
       >
-        <Text>Set score</Text>
+        <Text>Send message</Text>
       </TouchableOpacity>
+          </View>
+        )}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
