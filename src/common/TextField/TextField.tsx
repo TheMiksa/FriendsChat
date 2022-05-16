941be9273553ad78b0a41d4f7a1e0067aa5c039f
Mykhailo Hrytskov <mykhailo.hrytskov@dish.com>
2022-05-16 21:34:44 +0300

    TextField: added maxLength, errorMessage. Updated styles, refactoring

--- src/common/TextField/TextField.tsx
+++ src/common/TextField/TextField.tsx
@@ -1,34 +1,58 @@
-import { TextInput } from "react-native";
+import { View, TextInput, Text, StyleSheet } from "react-native";
 
 type TextFieldProps = {
   onChangeText: (value: string) => void,
   value: string,
-  style?: object,
+  style?: { errorMessage?: object, textInput?: object },
   placeholder?: string,
   multiline?: boolean,
+  maxLength?: number,
+  errorMessage?: string,
   props?: any,
 }
 
-const ownStyles = {
-  backgroundColor: '#E0E0E0',
-  width: 200,
-  borderRadius: 2,
-  padding: 4,
-  margin: 5,
-};
+const styles = StyleSheet.create({
+  container: {
+    margin: 5,
+    width: 200,
+  },
+  textInput: {
+    backgroundColor: '#E0E0E0',
+    borderRadius: 2,
+    padding: 4,
+  },
+  errorText: {
+    marginTop: 2,
+    textAlign: 'center',
+    color: 'tomato',
+  },
+});
 
 export const TextField: React.FC<TextFieldProps> = (
-  { onChangeText, value, style, placeholder, multiline = false, ...props},
+  { onChangeText, value, style, placeholder, multiline, maxLength, errorMessage, ...props},
   ) => (
-    <TextInput
+    <View style={styles.container}>
+      <TextInput
       onChangeText={onChangeText}
       value={value}
       placeholder={placeholder}
       multiline={multiline}
+      maxLength={maxLength}
       style={{
-        ...ownStyles,
-        ...style,
+        ...styles.textInput,
+        ...(style?.textInput || {}),
       }}
       {...props}
-    />
+      />
+      {!!errorMessage && (
+        <Text
+          style={{
+            ...styles.errorText,
+            ...(style?.errorMessage || {}),
+          }}
+        >
+          {errorMessage}
+        </Text>
+      )}
+    </View>
   );
