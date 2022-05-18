e812cf0462d86d235aca5848073ea981e3b01e89
Mykhailo Hrytskov <mykhailo.hrytskov@dish.com>
2022-05-19 00:05:53 +0300

    Updated styles and prop types, added title

--- src/common/ModalWindow/ModalWindow.tsx
+++ src/common/ModalWindow/ModalWindow.tsx
@@ -6,7 +6,8 @@ import {
   StyleProp, 
   ViewStyle, 
   StyleSheet,
-  TextStyle
+  TextStyle,
+  Text,
  } from "react-native";
 import { Button } from '../Button/Button';
 
@@ -16,11 +17,16 @@ type ModalWindowProps =  ModalProps & {
   onRightButtonPress?: () => void,
   leftButtonTitle?: string,
   rightButtonTitle?: string,
+  title?: string,
   styles?: {
     modal?: StyleProp<ViewStyle>,
     container?: StyleProp<ViewStyle>,
-    leftButton?: { button?: StyleProp<ViewStyle>, title?: StyleProp<TextStyle> },
-    rightButton?: { button?: StyleProp<ViewStyle>, title?: StyleProp<TextStyle> },
+    buttonsBock?: StyleProp<ViewStyle>,
+    leftButton?:StyleProp<ViewStyle> ,
+    leftButtonTitle?: StyleProp<TextStyle> ,
+    rightButton?: StyleProp<ViewStyle>,
+    rightButtonTitle?: StyleProp<TextStyle>,
+    title?: StyleProp<TextStyle>,
   },
 };
 
@@ -29,7 +35,26 @@ const ownStyles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(100, 100, 100, 0.8)',
+  },
+  buttonsBlock: {
+    flexDirection: 'row',
+    minWidth: 150,
+    minHeight: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(240, 240, 240, 0.9)',
+  },
+  leftButton: {
+
+  },
+  leftButtonTitle: {
+
+  },
+  rightButton: {
+
+  },
+  rightButtonTitle: {
+
   },
 });
 
@@ -42,6 +67,7 @@ export const ModaWindow: React.FC<ModalWindowProps> = (
     onRightButtonPress, 
     leftButtonTitle, 
     rightButtonTitle, 
+    title,
     ...props
   },
   ) => {
@@ -61,20 +87,33 @@ export const ModaWindow: React.FC<ModalWindowProps> = (
           {children ? (
             children
           ) : (
-            <>
+           <View>
+             {title && (
+               <Text style={styles?.title}>{title}</Text>
+             )}
+             <View
+              style={[ownStyles.buttonsBlock, styles?.buttonsBock]}
+            >
               <Button
                 onPress={onLeftButtonPress}
-                styles={styles?.leftButton}
+                styles={{
+                  button: StyleSheet.flatten([ownStyles.leftButton, styles?.leftButton]),
+                  title: StyleSheet.flatten([ownStyles.leftButtonTitle, styles?.leftButtonTitle]),
+                }}
               >
                 {leftButtonTitle || 'Cancel'}
               </Button>
               <Button
                 onPress={onRightButtonPress}
-                styles={styles?.rightButton}
+                styles={{
+                  button: StyleSheet.flatten([ownStyles.rightButton, styles?.rightButton]),
+                  title: StyleSheet.flatten([ownStyles.rightButtonTitle, styles?.rightButtonTitle]),
+                }}
               >
                 {rightButtonTitle || 'Confirm'}
               </Button>
-            </>
+            </View>
+           </View>
             )}
         </View>
       </TouchableWithoutFeedback>
